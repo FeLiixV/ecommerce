e898153a3a157f4abfad6a578c9b0c187ddea1b2
Mateus Felix <mateusfvilela@gmail.com>
2020-04-25 16:03:59 -0300

    tpl site

--- views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
+++ views-cache/index.c5b813be90a10a19f97859c318d75589.rtpl.php
@@ -1,1 +1,201 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?><h1>Olá Mundo!</h1>
+<?php if(!class_exists('Rain\Tpl')){exit;}?><div class="slider-area">
+    <!-- Slider -->
+    <div class="block-slider block-slider4">
+        <ul class="" id="bxslider-home4">
+            <li>
+                <img src="/res/site/img/motul1.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Motul <span class="primary">15W50 <strong>7100</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Perfeito para sua moto</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide2.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        by one, get one <span class="primary">50% <strong>off</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">school supplies & backpacks.*</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide3.png" alt="Slide">
+                <div class="caption-group">
+                    <h2 class="caption title">
+                        Apple <span class="primary">Store <strong>Ipod</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">Select Item</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+            <li><img src="/res/site/img/h4-slide4.png" alt="Slide">
+                <div class="caption-group">
+                  <h2 class="caption title">
+                        Apple <span class="primary">Store <strong>Ipod</strong></span>
+                    </h2>
+                    <h4 class="caption subtitle">& Phone</h4>
+                    <a class="caption button-radius" href="#"><span class="icon"></span>Comprar</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+    <!-- ./Slider -->
+</div> <!-- End slider area -->
+
+<div class="promo-area">
+<div class="zigzag-bottom"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-3 col-sm-6">
+            <div class="single-promo promo1">
+                <i class="fa fa-refresh"></i>
+                <p>1 ano de garantia</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="single-promo promo2">
+                <i class="fa fa-truck"></i>
+                <p>Frete grátis</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="single-promo promo3">
+                <i class="fa fa-lock"></i>
+                <p>Pagamento seguro</p>
+            </div>
+        </div>
+        <div class="col-md-3 col-sm-6">
+            <div class="single-promo promo4">
+                <i class="fa fa-gift"></i>
+                <p>Novos produtos</p>
+            </div>
+        </div>
+    </div>
+</div>
+</div> <!-- End promo area -->
+
+<div class="maincontent-area">
+<div class="zigzag-bottom"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="latest-product">
+                <h2 class="section-title">Produtos</h2>
+                <div class="product-carousel">
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m1.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2><a href="#">Motul 4T 15w50 7100</a></h2>
+                        
+                        <div class="product-carousel-price">
+                            <ins>$999.00</ins>
+                        </div> 
+                    </div>
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m3.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2>Filtro de Ar Fox/Gol/Voyage</h2>
+                        <div class="product-carousel-price">
+                            <ins>$999.99</ins>
+                        </div> 
+                    </div>
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m2.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2>Havoline SAE 5w30</h2>
+
+                        <div class="product-carousel-price">
+                            <ins>$0.00</ins>
+                        </div>                                 
+                    </div>
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m4.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2><a href="#">Shell Rimula RT4 X 15w40</a></h2>
+
+                        <div class="product-carousel-price">
+                            <ins>$0.00</ins>
+                        </div>                            
+                    </div>
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m5.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2>Castrol Edge Professional 0w20</h2>
+
+                        <div class="product-carousel-price">
+                            <ins>$0.00</ins>
+                        </div>                                 
+                    </div>
+                    <div class="single-product">
+                        <div class="product-f-image">
+                            <img src="/res/site/img/m1.png" alt="">
+                            <div class="product-hover">
+                                <a href="#" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> + CARRINHO</a>
+                                <a href="#" class="view-details-link"><i class="fa fa-link"></i> VER DETALHES</a>
+                            </div>
+                        </div>
+                        
+                        <h2><a href="#">Motul 4T 15w50 7100</a></h2>
+                        
+                        <div class="product-carousel-price">
+                            <ins>$999.00</ins>
+                        </div>                      
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div> <!-- End main content area -->
+
+<div class="brands-area">
+<div class="zigzag-bottom"></div>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="brand-wrapper">
+                <div class="brand-list">
+                    <img src="/res/site/img/logo1.png" alt="">
+                    <img src="/res/site/img/logo2.png" alt="">
+                    <img src="/res/site/img/logo3.png" alt="">
+                    <img src="/res/site/img/logo4.png" alt="">
+                    <img src="/res/site/img/logo5.png" alt="">
+                    <img src="/res/site/img/logo6.png" alt="">                        
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</div> <!-- End brands area -->
